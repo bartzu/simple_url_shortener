57562e4314118e5bbd8f3fd099e5ca4dbe1863db
bartzu <1bart.zu@gmail.com>
2020-02-18 16:20:03 +0100

    Added models and configured admin display.

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import FullURL, Shortcut
 
-# Register your models here.
+
+@admin.register(Shortcut)
+class ShortcutAdmin(admin.ModelAdmin):
+    list_display = ('value', 'full_url')
+
+class ShortcutInline(admin.TabularInline):
+    model = Shortcut
+
+@admin.register(FullURL)
+class LinkAdmin(admin.ModelAdmin):
+    inlines = (ShortcutInline,)
--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class FullURL(models.Model):
+    url = models.URLField(unique=True)
+    
+    def __str__(self):
+        return self.url
+
+
+class Shortcut(models.Model):
+    value = models.CharField(primary_key=True, unique=True, max_length=50)
+    full_url = models.ForeignKey(FullURL, on_delete=models.CASCADE, related_name='shortcuts')
+
+    def __str__(self):
+        return self.value
